47309e3801f7636cf1a30c6817fc88c818fd687e
Tom Cully <tom.cully@loyalty.co.nz>
2015-09-04 13:34:42 +1200

    Added partials to demo

--- examples/demo.py
+++ examples/demo.py
@@ -1,5 +1,32 @@
+import inspect, os, sys
 import stringtree
 
-node = stringtree.Node('g',10)
-node.add_horizontal_new('a',1)
-node.add_horizontal_new('z',2)
+print "StringTree Python Demo"
+print "----------------------"
+
+dr = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+dictionary = stringtree.Tree()
+
+print "Loading Dictionary..."
+
+f = open(dr+'/dictionary.txt', 'r')
+c = 0
+for ln in f:
+  dictionary.add(ln.strip(), c)
+  c += 1
+f.close()
+
+print "Loaded "+str(c)+" words."
+
+# Partial Matching
+print "-Partial Matching -----------------"
+print "-Type 'exit' to quit"
+while True:
+  x = raw_input("> ")
+  if x == "exit":
+    break
+  for i in dictionary.partials(x):
+    sys.stdout.write(i+" ")
+  print
+
